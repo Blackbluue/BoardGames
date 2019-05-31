ad9815e2a1e2a527ca447ce10169c108dbfe9684
usacys <usacys@local>
2019-05-31 15:02:22 -0400

    -Initial commit for tic tac toe player

---
+++ games/tictactoe/player.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""player module."""
+
+
+class Player:
+    """Docstring for Player."""
+
+    def __init__(self, token):
+        """Initialize object."""
+        self._token = token.upper()
+
+    def get_token(self):
+        """Return the token of this player."""
+        return self.get_token
