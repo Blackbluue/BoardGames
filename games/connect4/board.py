bc0acf3e4b33a4fa3a14d0bc7c5b1a887f23b04b
usacys <usacys@local>
2019-08-14 09:10:34 -0400

    -Add win conditions for connect 4

--- games/connect4/board.py
+++ games/connect4/board.py
@@ -15,10 +15,70 @@ class Board(games.base.board.Board):
         super().__init__(row_count=self.ROW_COUNT, col_count=self.COL_COUNT,
                          empty_cell=self.EMPTY_CELL)
 
-    def check_win(self, x, y):
+    def check_win(self, row, col):
         """Check if the game has ended."""
+        view = self.get_board_view()
+        # check if previous move caused a win on vertical line
+        start = 0  # start of check
+        for row_chk in range(start, self.ROW_COUNT - 3):  # loop through possible start points
+            if view[row_chk][col] == view[row_chk + 1][col] == view [row_chk + 2][col] == view [row_chk + 3][col]:
+                if view[row_chk][col] != self.EMPTY_CELL:
+                    return True
+        # check if previous move caused a win on horizontal line
+        start = 0  # start of check
+        for col_chk in range(start, self.COL_COUNT - 3):  # loop through possible start points
+            if view[row][col_chk] == view[row][col_chk + 1] == view [row][col_chk + 2] == view [row][col_chk + 3]:
+                if view[row][col_chk] != self.EMPTY_CELL:
+                    return True
+        # check if previous move was on a reverse diagonal and caused a win
+        if self._check_rev_diag(view, row, col):
+            return True
+        # check if previous move was on a forward diagonal and caused a win
+        if self._check_fwd_diag(view, row, col):
+            return True
         return False  # unfinished
 
+    def _check_rev_diag(self, view, row, col):
+        """Check reverse diagnals for win conditions."""
+        # reverse diagnals cannot cause a win in these positions, dont check
+        invalid_win = [(0, 4), (0, 5), (0, 6), (1, 5), (1, 6), (2, 6),
+                       (5, 0), (5, 1), (5, 2), (4, 0), (4, 1), (3, 0)]
+        if (row, col) in invalid_win:
+            return False
+        # get coord for start of diagnal (row, col)
+        row_chk, col_chk = row, col
+        while row_chk > 0 and col_chk > 0:
+            row_chk -= 1
+            col_chk -= 1
+        # increment start until start is 4 cells from diagnal edge of board
+        while row_chk <= self.ROW_COUNT - 4 and col_chk <=  self.COL_COUNT - 4:
+            if view[row_chk][col_chk] == view[row_chk + 1][col_chk + 1] == view [row_chk + 2][col_chk + 2] == view [row_chk + 3][col_chk + 3]:
+                if view[row_chk][col_chk] != self.EMPTY_CELL:
+                    return True
+            row_chk += 1
+            col_chk += 1
+        return False  # win conditions not met
+
+    def _check_fwd_diag(self, view, row, col):
+        """Check forward diagnals for win conditions."""
+        # forward diagnals cannot cause a win in these positions, dont check
+        invalid_win = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0),
+                       (5, 4), (5, 5), (5, 6), (4, 5), (4, 6), (3, 6)]
+        if (row, col) in invalid_win:
+            return False
+        # get coord for start of diagnal (row, col)
+        row_chk, col_chk = row, col
+        while row_chk > 0 and col_chk < self.COL_COUNT - 1:
+            row_chk -= 1
+            col_chk += 1
+        # increment start until start is 4 cells from diagnal edge of board
+        while row_chk <= self.ROW_COUNT - 4 and col_chk >=  3:
+            if view[row_chk][col_chk] == view[row_chk + 1][col_chk - 1] == view [row_chk + 2][col_chk - 2] == view [row_chk + 3][col_chk - 3]:
+                if view[row_chk][col_chk] != self.EMPTY_CELL:
+                    return True
+            row_chk += 1
+            col_chk -= 1
+        return False  # win conditions not met
 
     def place(self, token, col):
         """Place a token at the first open spot of the specified column.
@@ -30,7 +90,6 @@ class Board(games.base.board.Board):
                 super().place(token, row, col)  # place token in that cell
                 return (row, col)
         return None
-
 
     def __str__(self):
         """String representation of the object."""
